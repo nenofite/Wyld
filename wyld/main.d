d4ec4351bc38fe1ddd76973e83217148ce8d10ec
Ben Tucker <nenofite@gmail.com>
2012-05-24 15:46:46 -0700

    Added screen stack

--- wyld/main.d
+++ wyld/main.d
@@ -2,6 +2,7 @@ module wyld.main;
 
 import wyld.format;
 import wyld.layout;
+import wyld.screen;
 import wyld.worldgen;
 
 import core.thread: Thread, dur;
@@ -467,74 +468,10 @@ void main() {
   world.barMsg("Now run around like an idiot");
   world.barMsg("and explore!");
   
-  auto hud = mainView(world);
+  auto stack = new ScrStack();
+  stack ~= new MainScreen(world);
   
-  hud.draw(Box.Dim(0, 0, n.COLS, n.LINES));
-
-  bool cont = true;
-  while (cont) {
-    int key = n.getch();
-    n.flushinp();
-    switch (key) {
-      //case n.KEY_UP:
-      case '8':
-        world.player.upd = world.player.move(0, -1, world);
-        break;
-      //case n.KEY_DOWN:
-      case '2':
-        world.player.upd = world.player.move(0, 1, world);
-        break;
-      //case n.KEY_LEFT:
-      case '4':
-        world.player.upd = world.player.move(-1, 0, world);
-        break;
-      //case n.KEY_RIGHT:
-      case '6':
-        world.player.upd = world.player.move(1, 0, world);
-        break;
-      //case n.KEY_HOME:
-      case '7':
-        world.player.upd = world.player.move(-1, -1, world);
-        break;
-      //case n.KEY_PPAGE:
-      case '9':
-        world.player.upd = world.player.move(1, -1, world);
-        break;
-      //case n.KEY_B2:
-      case '5':
-        world.player.upd = new Update(100, null);
-        break;
-      //case n.KEY_END:
-      case '1':
-        world.player.upd = world.player.move(-1, 1, world);
-        break;
-      //case n.KEY_NPAGE:
-      case '3':
-        world.player.upd = world.player.move(1, 1, world);
-        break;
-      case 'Q':
-        cont = false;
-        break;
-      case n.KEY_RESIZE:
-        clearScreen();
-        break;
-      default:
-        world.barMsg(format("Unknown key: %d '%s'", key, cast(char) key));
-        break;
-    }
-    
-    n.attrset(n.COLOR_PAIR(Col.TEXT));
-    clearScreen();
-    hud.draw(Box.Dim(0, 0, n.COLS, n.LINES));
-    while (world.player.upd !is null) {
-      world.update();
-      hud.draw(Box.Dim(0, 0, n.COLS, n.LINES));
-      n.refresh();
-      Thread.sleep(dur!("nsecs")(500));
-    }
-    
-    n.refresh();
-  }
+  while (stack.length > 0) stack.update();
 }
 
 void initColor() {
