6c0d32d6d838658ad545e1c304495ca1acbf7942
Ben Tucker <nenofite@gmail.com>
2012-07-01 01:29:43 -0700

    Initial commit.

---
+++ main.d
@@ -0,0 +1,871 @@
+module main;
+
+import std.array: appender;
+import std.conv: ConvException;
+import std.format: formattedRead, formattedWrite;
+import math = std.math;
+import rand = std.random;
+import std.stdio;
+import std.string: chomp, toStringz;
+
+
+Game game;
+Player player;
+
+
+/// Separates a string of words separated by spaces into a list of those
+/// words
+///
+/// ---
+/// assert(words("  bob billo  hoi ") == ["bob", "billo", "hoi"]);
+/// ---
+string[] words(string clump) {
+  /// Start with a single, empty word
+  string[] words = [""];
+
+  /// Go through each character
+  foreach (c; clump) {
+    /// If it's a space, start on the next word
+    if (c == ' ') {
+      if (words[$-1].length > 0) {
+        words ~= "";
+      }
+    } else {
+      /// Otherwise, append it to the current word
+      words[$-1] ~= c;
+    }
+  }
+
+  /// If there is a leftover empty word on the end, drop it
+  if (words[$-1].length == 0) {
+    words = words[0 .. $-1];
+  }
+
+  return words;
+}
+
+
+/// The game world and shtuff
+class Game : Location {
+  Time time;
+  Command[] commands;
+
+
+  this() {
+    time = new Time();
+  }
+
+
+  void loop() {
+    while (true) {
+      foreach (ent; game.contained) {
+        ent.statUpdate();
+
+        if (ent.update !is null) {
+          auto isDone = ent.update.run();
+
+          if (isDone) {
+            ent.update = null;
+          }
+        }
+      }
+
+      game.time.increment();
+    }
+  }
+
+
+  string read(string prompt = "> ") {
+    write(prompt);
+    stdout.flush();
+
+    return chomp(readln());
+  }
+
+
+  void put(string msg) {
+    writeln(msg);
+  }
+
+
+  void prompt(A...)(string msg, const string fmt, A args) {
+    while (true) {
+      game.put(msg);
+
+      auto input = game.read("? ");
+      auto success = scan(input, fmt, args);
+
+      if (success) {
+        return;
+      } else {
+        game.put("Invalid choice.");
+      }
+    }
+  }
+}
+
+
+/// Keeps track of in-game time
+class Time {
+  int ticks;
+
+
+  void increment() {
+    ++ticks;
+  }
+
+
+  static int fromSeconds(int seconds) {
+    return seconds * 100;
+  }
+
+
+  static int fromMinutes(int minutes) {
+    return fromSeconds(minutes * 60);
+  }
+}
+
+
+/// Something that contains other Ents
+abstract class Location {
+  Entity[] contained;
+
+
+  void add(Entity ent) {
+    contained ~= ent;
+  }
+
+
+  void remove(Entity ent) {
+    contained.remove(ent);
+    ent.location = null;
+  }
+}
+
+
+/// Removes the first occurence of the given item from the list
+void remove(T)(ref T[] list, T item) {
+  T[] newList = new T[](list.length - 1);
+
+  bool found;
+  foreach (i, a; list) {
+    if (!found && a is item) {
+      found = true;
+    } else if (found) {
+      newList[i-1] = a;
+    } else {
+      newList[i] = a;
+    }
+  }
+
+  assert(found, "Element not found in list.");
+
+  list = newList;
+}
+
+
+abstract class Entity : Location {
+  string keyword;
+  Name name;
+  Location location;
+
+  Update update;
+
+
+  this(string keyword, Name name) {
+    this.keyword = keyword;
+    this.name = name;
+  }
+
+
+  void statUpdate() {}
+}
+
+
+struct Name {
+  string singular;
+  string posessive;
+}
+
+
+abstract class Update {
+  int time;
+
+  this(int time) {
+    this.time = time;
+  }
+
+
+  bool run() {
+    if (time > 0) {
+      --time;
+
+      return false;
+    } else {
+      apply();
+
+      return true;
+    }
+  }
+
+
+  void apply();
+}
+
+
+abstract class Creature : Entity {
+  Stat stamina;
+  int strength;
+  int coordination;
+  BodyPart torso;
+
+  this(string keyword, Name name, Stat stamina, int strength, int coordination, BodyPart torso) {
+    super(keyword, name);
+
+    this.stamina = stamina;
+    this.strength = strength;
+    this.coordination = coordination;
+    this.torso = torso;
+  }
+}
+
+
+struct Stat {
+  private int _amount;
+  int max;
+
+  alias amount this;
+
+  this(int amount, int max) {
+    _amount = amount;
+    this.max = max;
+  }
+
+  this(int max) {
+    this(max, max);
+  }
+
+
+  @property ref int amount() {
+    /// Clip _amount to 0 <= _amount <= max
+    if (_amount < 0) {
+      _amount = 0;
+    } else if (_amount > max) {
+      _amount = max;
+    }
+
+    return _amount;
+  }
+}
+
+
+class BodyPart : Entity {
+  Stat hp;
+  int size;
+  bool isCritical;
+  Tags tags;
+  BodyPart[] sub;
+
+  this(string keyword, Name name, Stat hp, int size, bool isCritical, Tags tags, BodyPart[] sub) {
+    super(keyword, name);
+    this.hp = hp;
+    this.size = size;
+    this.isCritical = isCritical;
+    this.tags = tags;
+    this.sub = sub;
+  }
+
+
+  BodyPart[] mirror(string[] prefixes) {
+    static BodyPart addPrefix(string prefix, BodyPart part) {
+      auto dup = new BodyPart(prefix ~ part.keyword,
+                              Name(prefix ~ part.name.singular, prefix ~ part.name.posessive),
+                              part.hp,
+                              part.size,
+                              part.isCritical,
+                              part.tags,
+                              part.sub.dup);
+
+      foreach (ref sub; dup.sub) {
+        sub = addPrefix(prefix, sub);
+      }
+
+      return dup;
+    }
+
+    BodyPart[] list;
+
+    foreach (prefix; prefixes) {
+      prefix ~= " ";
+
+      list ~= addPrefix(prefix, this);
+    }
+
+    return list;
+  }
+
+
+  static struct Tags {
+    bool canSee;
+
+    bool canHold;
+    Entity held;
+  }
+}
+
+
+class Weapon : Entity {
+  int weight;
+  HitMethod[] hitMethods;
+
+  this(string keyword, Name name, int weight, HitMethod[] hitMethods) {
+    super(keyword, name);
+
+    this.weight = weight;
+    this.hitMethods = hitMethods;
+  }
+}
+
+
+/// A way of attacking using a Weapon
+class HitMethod {
+  string name;
+  int hitArea;
+  float transfer;
+
+  this(string name, int hitArea, float transfer) {
+    this.name = name;
+    this.hitArea = hitArea;
+    this.transfer = transfer;
+  }
+}
+
+
+abstract class Command {
+  string keyword;
+
+  this(string keyword) {
+    this.keyword = keyword;
+  }
+
+
+  void run(Parameters);
+
+
+  static class Parameters {
+    string[] words;
+    int index;
+
+    this(string[] words) {
+      this.words = words;
+    }
+
+
+    bool readEntity(ref Entity ent) {
+      if (index >= words.length) {
+        return false;
+      }
+
+      ent = matchKeyword(game.contained, words[index]);
+
+      ++index;
+
+      return (ent !is null);
+    }
+
+
+    int choose(string prompt, string[] choices) {
+      while (true) {
+        game.put(prompt);
+
+        foreach (int i, choice; choices) {
+          game.put(fmt("%s) %s", i + 1, choice));
+        }
+
+        int choice;
+        bool worked = scan(game.read("# "), "%s", &choice);
+
+        --choice;
+
+        if (worked && choice >= 0 && choice < choices.length) {
+          return choice;
+        } else {
+          game.put("Invalid choice.");
+        }
+      }
+    }
+  }
+}
+
+
+abstract class Function {
+
+}
+
+
+class Grasp : Function {
+  Entity held;
+}
+
+
+class See : Function {
+
+}
+
+
+string fmt(A...)(const string fmt, A args) {
+  auto writer = appender!(string)();
+  formattedWrite(writer, fmt, args);
+  return writer.data;
+}
+
+
+bool scan(A...)(string input, const string fmt, A args) {
+  uint result;
+
+  try {
+    result = formattedRead(input, fmt, args);
+  } catch (ConvException e) {
+    return false;
+  }
+
+  return (result == args.length);
+}
+
+
+class Player : Creature {
+  this(Stat stamina, int strength, int coordination) {
+    auto torso = new BodyPart("torso", Name("torso", "torso's"), Stat(100), 220, true, BodyPart.Tags(), [
+      new BodyPart("head", Name("head", "head's"), Stat(20), 40, true, BodyPart.Tags(),
+        (new BodyPart("eye", Name("eye", "eye's"), Stat(10), 4, false, BodyPart.Tags(true), [])).mirror(["left", "right"]))
+    ]);
+
+    torso.sub ~= (new BodyPart("arm", Name("arm", "arm's"), Stat(30), 40, false, BodyPart.Tags(), [
+      new BodyPart("hand", Name("hand", "hand's"), Stat(20), 18, false, BodyPart.Tags(false, true), [])
+    ])).mirror(["left", "right"]);
+
+    torso.sub ~= (new BodyPart("leg", Name("leg", "leg's"), Stat(40), 120, false, BodyPart.Tags(), [
+      new BodyPart("foot", Name("foot", "foot's"), Stat(20), 26, false, BodyPart.Tags(), [])
+    ])).mirror(["left", "right"]);
+
+    super("me", Name("you", "your"), stamina, strength, coordination, torso);
+  }
+
+
+  void statUpdate() {
+    if (update is null) {
+      auto input = words(game.read());
+
+      if (input.length > 0) {
+        auto cmd = matchKeyword(game.commands, input[0]);
+
+        if (cmd !is null) {
+          string[] args;
+
+          if (input.length > 1) {
+            args = input[1 .. $];
+          }
+
+          cmd.run(new Command.Parameters(args));
+        } else {
+          game.put("What you talkin bout?");
+        }
+      }
+    }
+  }
+}
+
+
+class Wolf : Creature {
+  this() {
+    auto torso = new BodyPart("torso", Name("torso", "torso's"), Stat(100), 220, true, BodyPart.Tags(), [
+      new BodyPart("head", Name("head", "head's"), Stat(20), 40, true, BodyPart.Tags(),
+        (new BodyPart("eye", Name("eye", "eye's"), Stat(10), 4, false, BodyPart.Tags(true), [])).mirror(["left", "right"]))
+    ]);
+
+    torso.sub ~= (new BodyPart("leg", Name("leg", "leg's"), Stat(50), 100, false, BodyPart.Tags(), [
+      new BodyPart("foot", Name("foot", "foot's"), Stat(20), 40, false, BodyPart.Tags(), [])
+    ])).mirror(["front left", "front right", "back left", "back right"]);
+
+    super("wolf", Name("wolf", "wolf's"), Stat(15), 9, 7, torso);
+  }
+
+
+  void statUpdate() {
+
+  }
+}
+
+
+class SetupCommand : Command {
+  this() {
+    super("");
+  }
+
+
+  void run(Command.Parameters) {
+    game.put("Enter your stats:\n");
+
+    Stat stamina;
+    game.prompt("Stamina:", "%d/%d", &stamina.amount(), &stamina.max);
+
+    int strength;
+    game.prompt("Strength:", "%d", &strength);
+
+    int coordination;
+    game.prompt("Coordination:", "%d", &coordination);
+
+    player = new Player(stamina, strength, coordination);
+
+    game.add(player);
+
+
+  }
+}
+
+
+/// Matches among any type that has the string 'keyword' in it
+K matchKeyword(K)(K[] list, string keyword) {
+  auto matches = list;
+
+  /// Go through the chars of the keyword and chop off Ents that
+  /// don't match it
+  foreach (int i, c; keyword) {
+    K[] newMatches;
+
+    foreach (k; matches) {
+      if (k.keyword.length > i) {
+        if (k.keyword[i] == c) {
+          newMatches ~= k;
+        }
+      }
+    }
+
+    matches = newMatches;
+  }
+
+  if (matches.length > 0) {
+    return matches[0];
+  } else {
+    return null;
+  }
+}
+
+
+class LookCommand : Command {
+  this() {
+    super("look");
+  }
+
+
+  void run(Command.Parameters params) {
+    Entity target;
+    auto isTargeted = params.readEntity(target);
+
+    if (isTargeted) {
+      game.put(fmt("%s:", target.name.singular));
+
+      auto creature = cast(Creature) target;
+
+      if (creature !is null) {
+        assert(creature.torso !is null);
+
+        foreach (part; allParts(creature.torso)) {
+          string output = part.name.singular ~ "\t" ~ hpName(part.hp);
+
+          if (part.tags.canHold) {
+            if (part.tags.held !is null) {
+              output ~= "\tHolding: " ~ part.tags.held.name.singular;
+            } else {
+              output ~= "\tCan hold";
+            }
+          }
+
+          game.put(output);
+        }
+      }
+    } else {
+      game.put(fmt("Here there is %(%s, %).", game.contained));
+
+      if (player.contained.length > 0) {
+        game.put(fmt("You are holding %(%s, %).", player.contained));
+      }
+    }
+  }
+}
+
+
+class GrabCommand : Command {
+  this() {
+    super("grab");
+  }
+
+
+  void run(Command.Parameters params) {
+    Entity target;
+    auto isTargeted = params.readEntity(target);
+
+    if (isTargeted) {
+      BodyPart[] hands;
+
+      foreach (part; allParts(player.torso)) {
+        if (part.tags.canHold) {
+          hands ~= part;
+        }
+      }
+
+      string[] strHands = new string[](hands.length);
+
+      foreach (int i, ref str; strHands) {
+        str = hands[i].name.singular ~ "\t";
+
+        if (hands[i].tags.held !is null) {
+          str ~= "Holding: " ~ hands[i].tags.held.name.singular;
+        } else {
+          str ~= "Empty";
+        }
+      }
+
+      auto choice = params.choose("Grab with which hand:", strHands);
+
+      if (hands[choice].tags.held !is null) {
+        game.add(hands[choice].tags.held);
+        game.put("You drop " ~ hands[choice].tags.held.name.singular);
+      }
+
+      player.update = new GenUpdate(100, () {
+        game.remove(target);
+        hands[choice].tags.held = target;
+
+        game.put("You pick up " ~ target.name.singular);
+      });
+    } else {
+      game.put("Must specify a target.");
+    }
+  }
+}
+
+
+class AttackCommand : Command {
+  this() {
+    super("attack");
+  }
+
+
+  void run(Command.Parameters params) {
+    Creature target;
+
+    {
+      Entity ent;
+
+      auto hasTarget = params.readEntity(ent);
+
+      target = cast(Creature) ent;
+
+      if (!hasTarget || target is null) {
+        game.put("Need creature target");
+        return;
+      }
+    }
+
+    struct WeaponHitMethod {
+      Weapon weapon;
+      HitMethod hitMethod;
+    }
+
+    WeaponHitMethod hitMethod;
+
+    {
+      /// Choose weapon and hit method
+      WeaponHitMethod[] hitMethods;
+      string[] strHitMethods;
+
+      foreach (part; allParts(player.torso)) {
+        if (part.tags.canHold && part.tags.held !is null) {
+          auto weapon = cast(Weapon) part.tags.held;
+
+          if (weapon !is null) {
+            foreach (method; weapon.hitMethods) {
+              hitMethods ~= WeaponHitMethod(weapon, method);
+              strHitMethods ~= fmt("%s w/ %s - area: %s in^2, transfer: %s, weight: %s lbs",
+                                   method.name,
+                                   weapon.name.singular,
+                                   method.hitArea,
+                                   method.transfer,
+                                   weapon.weight);
+            }
+          }
+        }
+      }
+
+      auto choice = params.choose("Choose method:", strHitMethods);
+
+      hitMethod = hitMethods[choice];
+    }
+
+    BodyPart targetPart;
+
+    {
+      /// Choose body part to target
+      BodyPart[] parts = allParts(target.torso);
+      string[] strParts = new string[](parts.length);
+
+      foreach (int i, part; parts) {
+        strParts[i] = fmt("%s (%s) - %s in^2, %s%% %s%%",
+                          part.name.singular,
+                          hpName(part.hp),
+                          part.size,
+                          toPercent(probFullHit(hitMethod.weapon, hitMethod.hitMethod, player.coordination, part)),
+                          toPercent(probFullMiss(hitMethod.weapon, hitMethod.hitMethod, player.coordination, part)));
+      }
+
+      auto choice = params.choose(hitMethod.hitMethod.name ~ " where:", strParts);
+
+      targetPart = parts[choice];
+    }
+
+    int sp;
+
+    while (true) {
+      game.prompt(fmt("Expending how many SPs? min: %s, max: %s", hitMethod.weapon.weight / 10, player.stamina.amount), "%d", &sp);
+
+      if (sp >= hitMethod.weapon.weight / 10 && sp <= player.stamina) {
+        break;
+      } else {
+        game.put("Not within range.");
+      }
+    }
+
+    auto dmg = calcDamage(hitMethod.weapon, hitMethod.hitMethod, player.coordination, targetPart, sp);
+    
+    targetPart.hp -= dmg;
+
+    game.put(fmt("You do %s damage to %s %s, leaving %s HPs.", dmg, target.name.posessive, targetPart.name.singular, targetPart.hp));
+
+    if (targetPart.hp.amount == 0) {
+      game.put("It falls off!");
+    }
+
+    player.stamina -= sp;
+    game.put(fmt("\nYou are left with %s SPs.", player.stamina));
+  }
+}
+
+
+float probFullHit(Weapon weapon, HitMethod hitMethod, int coordination, BodyPart target) {
+  return (target.size - hitMethod.hitArea) /
+         (100 / coordination);
+}
+
+
+float probFullMiss(Weapon weapon, HitMethod hitMethod, int coordination, BodyPart target) {
+  return (1 - (target.size / (100 / coordination)));
+}
+
+
+float calcDamage(Weapon weapon, HitMethod hitMethod, int coordination, BodyPart target, int sp) {
+  auto leftpoint = rand.uniform(0, 100) / player.coordination;
+  auto rightpoint = leftpoint + hitMethod.hitArea;
+
+  float effectiveness;
+
+  if (leftpoint >= target.size) {
+    effectiveness = 0;
+  } else if (rightpoint <= target.size) {
+    effectiveness = 1;
+  } else {
+    effectiveness = (target.size - leftpoint) / hitMethod.hitArea;
+  }
+
+  auto force = (sp - weapon.weight / 10) * (weapon.weight / 10) * hitMethod.transfer * effectiveness;
+
+  return force * 10 / hitMethod.hitArea;
+}
+
+
+float toPercent(float frac) {
+  if (frac <= 0) return 0;
+  if (frac >= 1) return 100;
+
+  int thousands = cast(int) frac * 1000;
+
+  return thousands / 10;
+}
+
+
+BodyPart[] allParts(BodyPart base) {
+  auto parts = [base];
+
+  foreach (part; base.sub) {
+    parts ~= allParts(part);
+  }
+
+  return parts;
+}
+
+
+string hpName(Stat hp) {
+  if (hp == 0) {
+    return "gone";
+  }
+
+  float ratio = hp / hp.max;
+
+  if (ratio >= .75) {
+    return "fine";
+  } else if (ratio >= .5) {
+    return "light";
+  } else if (ratio >= .25) {
+    return "moderate";
+  } else {
+    return "severe";
+  }
+}
+
+
+class GenUpdate : Update {
+  void delegate() applyF;
+
+  this(int time, void delegate() applyF) {
+    super(time);
+
+    this.applyF = applyF;
+  }
+
+
+  void apply() {
+    if (applyF !is null) {
+      applyF();
+    }
+  }
+}
+
+
+class HeavyStick : Weapon {
+  this() {
+    super("stick", Name("heavy stick", "heavy stick's"), 20, [
+      new HitMethod("whack", 6, 0.6),
+      new HitMethod("jab", 2, 0.8)
+    ]);
+  }
+}
+
+
+void main() {
+  game = new Game();
+
+  game.commands = [
+    new LookCommand(),
+    new AttackCommand(),
+    cast(Command) new GrabCommand()
+  ];
+
+  auto setup = new SetupCommand();
+  setup.run(new Command.Parameters([]));
+
+  game.add(new HeavyStick());
+
+  game.add(new Wolf());
+
+  game.loop();
+}
